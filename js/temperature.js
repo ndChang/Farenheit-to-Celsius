f074793365d1b7fe59a8511daf3af06a2beb4bdb
ndChang <52163387+ndChang@users.noreply.github.com>
2019-08-14 21:30:46 -0400

    Start-up

---
+++ js/temperature.js
@@ -0,0 +1,13 @@
+// User should be able to type in a temperature in Farenheit
+//That temperature should be converted to celcius and printed on page
+// background color/image should change based on ranges
+// -20 => 0 winter
+// 0 => 15 spring
+// >15 summer
+
+//math for f to c is f - 32 *5/9;
+//input in f and we convert to c
+
+function conversion() {
+    let farenheit = parseInt($('#'))
+}
